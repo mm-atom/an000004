c17ed34dd62eda84e347dc7b4dfe641129161e13
taoqf <tao_qf@163.com>
2020-03-07 12:58:57 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export default function fun() {
+export interface IWebResult {
+	data: Buffer | unknown;
+	cookie?: { [name: string]: string } | null;
+	content_type?: string;
+	headers?: {
+		[key: string]: string[];
+	};
+	attachment?: string;
+	redirect?: string;
+	status_code?: number;
 }
